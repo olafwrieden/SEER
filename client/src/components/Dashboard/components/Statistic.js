ebeb28857cc5872f1bdbf26e06c1ef9f8fd2b263
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-12 22:52:17 +1200

    Styled Dashboard

--- client/src/components/Dashboard/components/Statistic.js
+++ client/src/components/Dashboard/components/Statistic.js
@@ -1,14 +1,14 @@
 import React from "react";
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
 
-const Statistic = ({ value, text }) => (
-  <div className="tile is-ancestor has-text-centered">
+const Statistic = ({ value, text, bgcolor }) => (
+  <div className="tile is-ancestor has-text-left">
     <div className="tile is-parent">
-      <article className="tile is-child box notification is-dark">
-        <p className="title">
+      <article className={`tile is-child box ${bgcolor}`}>
+        <p className="title has-text-white">
           <CountUp end={value} />
         </p>
-        <p className="subtitle">{text}</p>
+        <p className="subtitle has-text-white">{text}</p>
       </article>
     </div>
   </div>
--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -6,18 +6,40 @@ const Dashboard = () => {
     <>
       <section className="section">
         <div className="container">
+          {/* User Greeting */}
+          <h1 className="title has-text-grey-light">
+            Hello <span className="has-text-dark">John Appleseed</span>
+          </h1>
+
+          {/* Platform Statistics */}
           <div className="columns is-multiline">
             <div className="column is-6-tablet is-3-desktop">
-              <Statistic value={8365} text="Total Entries" />
+              <Statistic
+                value={8365}
+                text="Total Entries"
+                bgcolor="has-background-link"
+              />
             </div>
             <div className="column is-6-tablet is-3-desktop">
-              <Statistic value={392} text="Total Users" />
+              <Statistic
+                value={24}
+                text="Pending Analysis"
+                bgcolor="has-background-info"
+              />
             </div>
             <div className="column is-6-tablet is-3-desktop">
-              <Statistic value={2172} text="In Moderation" />
+              <Statistic
+                value={2172}
+                text="In Moderation"
+                bgcolor="has-background-danger"
+              />
             </div>
             <div className="column is-6-tablet is-3-desktop">
-              <Statistic value={24} text="Pending Analysis" />
+              <Statistic
+                value={392}
+                text="Total Users"
+                bgcolor="has-background-success"
+              />
             </div>
           </div>
         </div>
