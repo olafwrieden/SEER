34f3b681ec7771532ff3f4daff2ffd42efe5a688
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-12 22:24:06 +1200

    Modified <App /> Test

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("Renders 'Welcome to SEER'", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const HeroElement = getByText(/Welcome to SEER/i);
+  expect(HeroElement).toBeInTheDocument();
+});
+
+test("Renders 'The Team'", () => {
+  const { getByText } = render(<App />);
+  const TeamElement = getByText(/The Team/i);
+  expect(TeamElement).toBeInTheDocument();
 });
