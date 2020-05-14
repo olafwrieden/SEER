baaffaed654db161b5da3f25fbffff6f8f493bd3
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-15 00:07:57 +1200

    Added Document Schema

---
+++ api/app/models/Book.js
@@ -0,0 +1,64 @@
+const { Schema } = require('mongoose');
+const AuthorSchema = require('./Name');
+
+/**
+ * The schema for a Book
+ */
+const BookSchema = new Schema({
+  authors: {
+    type: [AuthorSchema],
+    required: [true, 'An author is required.']
+  },
+  title: {
+    type: String,
+    required: [true, 'A title is required.']
+  },
+  city: {
+    type: String,
+    required: [true, 'A city is required.']
+  },
+  state: {
+    type: String
+  },
+  country: {
+    type: String
+  },
+  publisher: {
+    type: String,
+    required: [true, 'A publisher is required.']
+  },
+  year: {
+    type: Number,
+    required: [true, 'A year is required.']
+  },
+  volume: {
+    type: Number
+  },
+  number_of_volumes: {
+    type: Number
+  },
+  pages: {
+    type: String,
+    required: [true, 'Page numbers are required.']
+  },
+  editor: {
+    type: [AuthorSchema]
+  },
+  translator: {
+    type: [AuthorSchema]
+  },
+  short_title: {
+    type: String
+  },
+  standard_number: {
+    type: Number
+  },
+  edition: {
+    type: String
+  },
+  comments: {
+    type: String
+  }
+});
+
+module.exports = BookSchema;
---
+++ api/app/models/Name.js
@@ -0,0 +1,23 @@
+const { Schema } = require('mongoose');
+
+/**
+ * The schema for an Author / Translator / Editor / Producer
+ */
+const NameSchema = new Schema({
+  first_name: {
+    type: String,
+    match: [/^[a-zA-Z]+$/, 'First Name is invalid.'],
+    required: [true, 'First Name is required.']
+  },
+  middle_name: {
+    type: String,
+    match: [/^[a-zA-Z]+$/, 'Middle Name is invalid.']
+  },
+  last_name: {
+    type: String,
+    match: [/^[a-zA-Z]+$/, 'Last Name is invalid.'],
+    required: [true, 'Last Name is required.']
+  }
+});
+
+module.exports = NameSchema;
