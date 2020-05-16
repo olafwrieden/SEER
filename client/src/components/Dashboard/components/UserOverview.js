b8132064505db5e5a32d72a1af227b67b270df4e
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-17 00:00:48 +1200

    Added New User Modal

--- client/src/components/Dashboard/components/UserOverview.js
+++ client/src/components/Dashboard/components/UserOverview.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiUserPlus, FiUsers } from "react-icons/fi";
+import NewUser from "./NewUser";
 
 const Counter = ({ title, count }) => (
   <div className="column">
@@ -10,43 +11,56 @@ const Counter = ({ title, count }) => (
   </div>
 );
 
-const UserOverview = ({ standard, moderators, analysts, admins, actions }) => (
-  <div className="box has-text-centered">
-    <h2 className="subtitle">User Overview</h2>
+const UserOverview = ({ standard, moderators, analysts, admins, actions }) => {
+  const [showNewUserModal, setShowNewUserModal] = useState(false);
+  const toggleNewUserModal = () => setShowNewUserModal(!showNewUserModal);
 
-    {/* Statistics */}
-    <div className="columns is-mobile is-multiline">
-      <Counter title="Standard" count={standard} />
-      <Counter title="Moderators" count={moderators} />
-    </div>
-    <div className="columns is-mobile is-multiline">
-      <Counter title="Analysts" count={analysts} />
-      <Counter title="Admins" count={admins} />
-    </div>
+  return (
+    <>
+      <div className="box has-text-centered">
+        <h2 className="subtitle">User Overview</h2>
 
-    {/* Actions */}
-    {actions && (
-      <div className="buttons has-addons is-centered is-paddingless">
-        <div
-          className="columns is-mobile is-multiline is-gapless"
-          style={{ width: "100%" }}
-        >
-          <button className="column button is-primary is-light is-fullwidth">
-            <span className="icon">
-              <FiUserPlus />
-            </span>
-            <span>New</span>
-          </button>
-          <button className="column button is-link is-light is-fullwidth">
-            <span className="icon">
-              <FiUsers />
-            </span>
-            <span>View</span>
-          </button>
+        {/* Statistics */}
+        <div className="columns is-mobile is-multiline">
+          <Counter title="Standard" count={standard} />
+          <Counter title="Moderators" count={moderators} />
         </div>
+        <div className="columns is-mobile is-multiline">
+          <Counter title="Analysts" count={analysts} />
+          <Counter title="Admins" count={admins} />
+        </div>
+
+        {/* Actions */}
+        {actions && (
+          <div className="buttons has-addons is-centered is-paddingless">
+            <div
+              className="columns is-mobile is-multiline is-gapless"
+              style={{ width: "100%" }}
+            >
+              <button
+                className="column button is-primary is-light is-fullwidth"
+                onClick={toggleNewUserModal}
+              >
+                <span className="icon">
+                  <FiUserPlus />
+                </span>
+                <span>New</span>
+              </button>
+              <button className="column button is-link is-light is-fullwidth">
+                <span className="icon">
+                  <FiUsers />
+                </span>
+                <span>View</span>
+              </button>
+            </div>
+          </div>
+        )}
       </div>
-    )}
-  </div>
-);
+
+      {/* New User Modal */}
+      <NewUser isOpen={showNewUserModal} toggle={toggleNewUserModal} />
+    </>
+  );
+};
 
 export default UserOverview;
