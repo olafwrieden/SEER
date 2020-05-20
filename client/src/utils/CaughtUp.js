10edfabe0e889f315e51146dc07eb2243907b616
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-20 23:34:13 +1200

    Updated Icon

--- client/src/utils/CaughtUp.js
+++ client/src/utils/CaughtUp.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { BsCheckCircle } from "react-icons/bs";
 
 /**
  * Shows `You're All Caught Up` message.
  */
 const CaughtUp = ({ children }) => (
   <div className="has-text-centered">
-    <i
-      className="far fa-check-circle fa-4x has-text-primary"
-      aria-hidden="true"
+    <BsCheckCircle
+      size="4em"
+      className="has-text-primary"
       style={{ marginBottom: "10px" }}
-    ></i>
+    />
     <p className="subtitle has-text-centered">You're All Caught Up</p>
     <p className="has-text-centered">{children}</p>
   </div>
