3758cdd8f205c6b22b1d9983c430ae7daa5fa041
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-20 17:55:50 +1200

    Formatting + Fixed Warning

--- client/src/components/Search/components/Results.js
+++ client/src/components/Search/components/Results.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import { RecordType } from "../../utils/RecordType";
+import React from "react";
 
-const Results = ({ title, type, date, author,descriptor}) => {
-    return (
-        <div className="box has-background-light">
-        <div className="columns is-gapless">
-          {/* Icon */}
-          <div className="column is-one-fifth">
-            <i className={type.icon} aria-hidden="true"></i>
-          </div>
-          
-          <div className="column">
-            <strong>{title}</strong>
-          </div>
+const Results = ({ title, type, date, author, descriptor }) => {
+  return (
+    <div className="box has-background-light">
+      <div className="columns is-gapless">
+        {/* Icon */}
+        <div className="column is-one-fifth">
+          <i className={type.icon} aria-hidden="true"></i>
         </div>
-        
-        <div className="columns is-gapless">
-          <div className="column is-one-fifth">{date}</div>
-          <div className="column">{descriptor}</div>
-          <div className="column is-one-fifth">
-            <p>{author}</p>
-          </div>
+
+        <div className="column">
+          <strong>{title}</strong>
         </div>
       </div>
-    );
-}
+
+      <div className="columns is-gapless">
+        <div className="column is-one-fifth">{date}</div>
+        <div className="column">{descriptor}</div>
+        <div className="column is-one-fifth">
+          <p>{author}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Results;
--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -11,16 +11,17 @@ const search_results = [
     type: RecordType.BOOK,
     date: "June 2016",
     author: "Wilson, B., Adolfo, N. S. S. G., & Emer, P. F. C. M.",
-    descriptor: "Analyzed the conclusions of previously published articles on the effects of TDD on internal and external software quality and productivity, comparing TDD with Test Last Development (TLD).",
+    descriptor:
+      "Analyzed the conclusions of previously published articles on the effects of TDD on internal and external software quality and productivity, comparing TDD with Test Last Development (TLD).",
   },
   {
     id: 2,
-    name:
-      "Test-Driven Development – What is it and how do you use it?",
+    name: "Test-Driven Development – What is it and how do you use it?",
     type: RecordType.ARTICLE,
     date: "11 April 2017",
     author: "Powell-Morse, A.",
-    descriptor: "Explore the fundamental components of test-driven development, including the basic test-driven development life cycle, some best practices, and potential advantages and disadvantages to implemented test-driven development in your own projects.",
+    descriptor:
+      "Explore the fundamental components of test-driven development, including the basic test-driven development life cycle, some best practices, and potential advantages and disadvantages to implemented test-driven development in your own projects.",
   },
   {
     id: 3,
@@ -28,16 +29,17 @@ const search_results = [
     type: RecordType.UNCLASSIFIED,
     date: "2 July 2018",
     author: "Koutifaris, A.",
-    descriptor: "Fundamentals of Test Driven Development, addressing common misconceptions about the TDD technique. ",
+    descriptor:
+      "Fundamentals of Test Driven Development, addressing common misconceptions about the TDD technique. ",
   },
   {
     id: 4,
-    name:
-      "Test-Driven Development: Really, It’s a Design Technique.",
+    name: "Test-Driven Development: Really, It’s a Design Technique.",
     type: RecordType.WEBSITE,
     date: "10 May 2019",
     author: "Taman, M., & Bryant, D.",
-    descriptor: "Test-driven development (TDD) is an established technique for delivering better software, more rapidly, and more sustainably over time.",
+    descriptor:
+      "Test-driven development (TDD) is an established technique for delivering better software, more rapidly, and more sustainably over time.",
   },
 ];
 
@@ -47,29 +49,27 @@ const Search = () => {
       <section className="section">
         <div className="container">
           <h2 className="title">Results</h2>
-          <p className="subtitle">
-            Here are your results from your search.
-          </p>
+          <p className="subtitle">Here are your results from your search.</p>
         </div>
       </section>
 
       <section className="section">
         <div className="container">
-          {search_results.map(({ id, name, type, date, author, descriptor }) => (
-            <Results
-              key={id}
-              title={name}
-              type={type}
-              date={date}
-              author={author}
-              descriptor={descriptor}
-            />
-          ))}
+          {search_results.map(
+            ({ id, name, type, date, author, descriptor }) => (
+              <Results
+                key={id}
+                title={name}
+                type={type}
+                date={date}
+                author={author}
+                descriptor={descriptor}
+              />
+            )
+          )}
 
           {!search_results.length && (
-            <CaughtUp>
-              Results from your search will be shown here.
-            </CaughtUp>
+            <CaughtUp>Results from your search will be shown here.</CaughtUp>
           )}
         </div>
       </section>
