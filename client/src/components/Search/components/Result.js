d60b1f1b8753f68e7d24d352b77c0f621ac754e7
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-20 23:35:18 +1200

    Updated Results View

--- client/src/components/Search/components/Result.js
+++ client/src/components/Search/components/Result.js
@@ -1,24 +1,79 @@
 import React from "react";
+import { AiFillStar } from "react-icons/ai";
 
-const Result = ({ title, type, date, author, descriptor }) => {
+const Result = ({
+  title,
+  type,
+  date,
+  authors,
+  researchQuestion,
+  outcome,
+  rating,
+  seMethod,
+}) => {
   return (
     <div className="box">
-      <div className="columns is-gapless">
+      <div className="columns">
         {/* Icon */}
         <div className="column is-one-fifth">
-          <i className={type.icon} aria-hidden="true"></i>
+          <i className={`${type.icon} has-text-primary`} aria-hidden="true"></i>
         </div>
 
+        {/* Title & Author Info */}
         <div className="column">
-          <strong>{title}</strong>
+          <div className="columns">
+            <div className="column">
+              <p>
+                <strong>{title}</strong>
+              </p>
+              <br />
+              <div className="tags">
+                {authors.map((author, i) => (
+                  <span key={i} className="tag">
+                    {author}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="columns is-gapless">
-        <div className="column is-one-fifth">{date}</div>
-        <div className="column">{descriptor}</div>
+      <div className="columns">
+        {/* Type, Date, Rating */}
         <div className="column is-one-fifth">
-          <p>{author}</p>
+          {type.name}
+          <br />
+          {date}
+          <br />
+          <div>
+            <span className="icons has-text-warning">
+              {[...Array(rating)].map((_, i) => (
+                <AiFillStar key={i} />
+              ))}
+            </span>
+          </div>
+        </div>
+
+        {/* Research Question & Outcome */}
+        <div className="column">
+          <p>
+            <strong>Question:</strong> {researchQuestion}
+          </p>
+          <p>
+            <strong>Outcome:</strong> {outcome}
+          </p>
+        </div>
+
+        {/* SE Method */}
+        <div className="column is-one-fifth">
+          <div className="tags">
+            {seMethod.map((method, i) => (
+              <span key={i} className="tag">
+                {method}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
