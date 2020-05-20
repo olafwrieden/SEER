792232d2e4f3ee9ca667d8704fed5ee5e1347b8c
Breahem-Bedi <lovelybedi@hotmail.com>
2020-05-21 02:23:06 +1200

    Reset the Css for the Search Bar

--- client/src/components/Search/components/SearchBar.js
+++ client/src/components/Search/components/SearchBar.js
@@ -20,15 +20,26 @@ class SearchBar extends Component
 
    render()
    {
+        
         return(<div className = "search_Bar">
-        <form>
-        <label class="label">Description</label>   
-        
-        <input class="input" type="text" placeholder="Article Description" style = {{width: 500}}></input>
-        
+                <>
+      {/* Search Terms */}
+      <div className="columns is-multiline box is-desktop is-paddingless">
+        <div className="column is-6-desktop is-12-tablet split-border">
+          <label className="label search-label">Search Terms</label>
+          <input
+            id="Description"
+            className="input search-input"
+            placeholder="TDD + Waterfall + Productivity"
+            type="text"
+          />
+        </div>
+
+        {/* Year From */}
+        <div className="column is-6-desktop is-12-tablet split-border">
         <label class="label">Date Range From </label>  
         <DateRangePicker
-                style = {{width: 500}}
+                //style = {{width: 1000}}
                 startDate={this.state.startDate} // momentPropTypes.momentObj or null,
                 startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
                 endDate={this.state.endDate} // momentPropTypes.momentObj or null,
@@ -37,10 +48,18 @@ class SearchBar extends Component
                 focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                 onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
         />
+        </div>
 
-        <label class="label">IF:</label>
-        <div class="select">
+      </div>
+    </>
+        <form>     
+        <>
+      {/* Search Terms */}
+      <div className="columns is-multiline box is-desktop is-paddingless">
+      <label class="label">IF: </label>
+      <div class="select">
                 <select>
+                    <option>Name of Field</option>
                     <option>Option 1</option>
                     <option>Option 2</option>
                     <option>Option 3</option>
@@ -49,38 +68,64 @@ class SearchBar extends Component
                     <option>Option 6</option>
                 </select>
         </div>
-        <div class="select">
-                <select>
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                    <option>Option 4</option>
-                    <option>Option 5</option>
-                    <option>Option 6</option>
-                </select>
-        </div>
-        <div class="select">
-                <select>
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                    <option>Option 4</option>
-                    <option>Option 5</option>
-                    <option>Option 6</option>
-                </select>
-        </div>
-        <h3>Reset or Add Button </h3>
-        <button
-          onClick={this.resetForm}
-          type="reset"> - </button>
-        <button
-          //onClick={this.addForm}
-          type="button"> + </button>
-          <h3>  </h3>
-          <h3> ----- </h3>
-        <button class="button is-link">Search</button>
 
+        {/* Year From */}
+        <div class="select">
+                <select>
+                    <option>Operator</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                    <option>Option 5</option>
+                    <option>Option 6</option>
+                </select>
+        </div>
+
+        {/* Year To */}
+        <div class="select">
+                <select>
+                <option>Value</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                    <option>Option 5</option>
+                    <option>Option 6</option>
+                </select>
+        </div>
+
+        {/* Search Button */}
+        <div className="column is-2-desktop is-12-tablet is-12-mobile is-paddingless">
+          <button className="Add Button">
+            +
+          </button>
+          <button className="Reset Button">
+            -
+          </button>
+        </div>
+      </div>
+    </>
+
+    <>
+      {/* Search Terms */}
+      <div className="columns is-multiline box is-desktop is-paddingless">
+        {/* Search Button */}
+        <div className="column is-2-desktop is-12-tablet is-12-mobile is-paddingless">
+          <button className="Run Search Button">
+            Run Search
+          </button>
+          <button className="Save Search Button">
+            Save Search
+          </button>
+          <button className="Cancel Button">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </>
         </form>
+        
          </div>);
    }
 }    
