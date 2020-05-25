fff01f2e48f63c62263c73368e203d200f3f4575
KJ Millar <kjmillar.webdesigner@gmail.com>
2020-05-25 18:06:02 +1200

    i think i fixed the layout of the initial suggest page

--- client/src/components/Suggestion/components/Category.js
+++ client/src/components/Suggestion/components/Category.js
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 const Category = ({ type }) => (
   <div className="box has-background-info" style={{ cursor: "pointer" }}>
     <div className="columns is-gapless is-flex is-vcentered is-centered has-text-white">
-      <Link to={`/suggest/${type.url}`}>
       {/* Icon */}
       <div className="column is-one-fifth">
+      <Link to={`/suggest/${type.url}`} className="has-text-white">
         <i className={type.icon} aria-hidden="true"></i>
+        </Link>
       </div>
       {/* Title */}
       <div className="column has-text-white">
+      <Link to={`/suggest/${type.url}`}>
         <strong className="subtitle has-text-white">{type.name}</strong>
+        </Link>
       </div>
-      </Link>
     </div>
   </div>
 );
