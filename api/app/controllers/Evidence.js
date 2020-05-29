41da1b2e9d7b543f5df06c0fb3514e20c2b3e6fb
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-05-29 16:47:53 +1200

    Removed logging

--- api/app/controllers/Evidence.js
+++ api/app/controllers/Evidence.js
@@ -25,7 +25,6 @@ exports.createEvidenceReview = async (req, res) => {
   try {
     const id = await isIDValid(req.params.id);
     const author = req.user.id || null;
-    console.log(author);
     if (!author) {
       return handleError(res, 'You must be logged in to leave a review.');
     }
