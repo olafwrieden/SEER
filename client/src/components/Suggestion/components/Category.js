7f3c2254a4d0d5ec891b072b0d5628403b8fd5b7
KJ Millar <kjmillar.webdesigner@gmail.com>
2020-06-03 11:09:38 +1200

    wrap element in link correctly

--- client/src/components/Suggestion/components/Category.js
+++ client/src/components/Suggestion/components/Category.js
@@ -3,20 +3,16 @@ import { Link } from "react-router-dom";
 
 const Category = ({ type }) => (
   <div className="box has-background-info" style={{ cursor: "pointer" }}>
-    <div className="columns is-gapless is-flex is-vcentered is-centered has-text-white">
+    <Link to={`/suggest/${type.url}`} className="columns is-gapless is-flex is-vcentered is-centered has-text-white" >
       {/* Icon */}
       <div className="column is-one-fifth">
-      <Link to={`/suggest/${type.url}`} className="has-text-white">
         <i className={type.icon} aria-hidden="true"></i>
-        </Link>
       </div>
       {/* Title */}
-      <div className="column has-text-white">
-      <Link to={`/suggest/${type.url}`}>
+      <div className="column">
         <strong className="subtitle has-text-white">{type.name}</strong>
-        </Link>
       </div>
-    </div>
+    </Link>
   </div>
 );
 
