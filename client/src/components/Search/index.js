9b376b4619217361e13d05511830f4721b4588ed
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-06-03 23:23:11 +1200

    Added Initial Filter Layout

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -3,8 +3,10 @@ import { averageRatings, formatDate } from "../../utils/helpers";
 import { RecordType } from "../../utils/RecordType";
 import Result from "./components/Result";
 import SearchBar from "./components/SearchBar";
+import Filter from "./components/Filter";
 
 const Search = ({ terms, dateFrom, dateTo }) => {
+  const [filters, setFilters] = useState([]);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [pageCount, setPageCount] = useState(0);
@@ -34,6 +36,22 @@ const Search = ({ terms, dateFrom, dateTo }) => {
     fetchData({ pageIndex, pageSize });
   }, [fetchData, pageIndex, pageSize]);
 
+  const addFilter = (defautmethod, defaultOperator, defaultValue) => {
+    defautmethod = "method";
+    defaultOperator = "operator";
+    defaultValue = "value";
+
+    setFilters((filters) => [
+      ...filters,
+      { method: defautmethod, operator: defaultOperator, value: defaultValue },
+    ]);
+  };
+
+  const removeFilter = (e, index) => {
+    e.preventDefault();
+    setFilters([...filters.filter((_, filterIndex) => filterIndex !== index)]);
+  };
+
   return (
     <>
       <section className="section">
@@ -43,39 +61,49 @@ const Search = ({ terms, dateFrom, dateTo }) => {
             Verify a claim by searching our curated repository.
           </p>
           <SearchBar />
+
+          {/* Filters */}
+          {filters.map((_, index) => (
+            <Filter key={index} remove={(e) => removeFilter(e, index)} />
+          ))}
+          {/* Add Filters */}
+          <button
+            className="button is-small is-primary is-outlined"
+            onClick={() => addFilter()}
+          >
+            Add Filter
+          </button>
         </div>
       </section>
 
       {/* Search Results */}
-      <section className="section">
-        <div className="container">
-          {data.map((result) => {
-            const date = formatDate(result?.day, result?.month, result?.year);
-            const stars = averageRatings(result.ratings);
+      <div className="container">
+        {data.map((result) => {
+          const date = formatDate(result?.day, result?.month, result?.year);
+          const stars = averageRatings(result.ratings);
 
-            return (
-              <Result
-                key={result.id}
-                id = {result.id}
-                title={result.title}
-                type={RecordType[result.__type]}
-                date={date}
-                authors={result.authors}
-                researchQuestion={result.research_question}
-                outcome={result.outcome}
-                rating={stars}
-                seMethod={result.se_method}
-              />
-            );
-          })}
+          return (
+            <Result
+              key={result.id}
+              id={result.id}
+              title={result.title}
+              type={RecordType[result.__type]}
+              date={date}
+              authors={result.authors}
+              researchQuestion={result.research_question}
+              outcome={result.outcome}
+              rating={stars}
+              seMethod={result.se_method}
+            />
+          );
+        })}
 
-          {!data.length && (
-            <p className="has-text-centered">
-              Your search results will appear here.
-            </p>
-          )}
-        </div>
-      </section>
+        {!data.length && (
+          <p className="has-text-centered">
+            Your search results will appear here.
+          </p>
+        )}
+      </div>
     </>
   );
 };
